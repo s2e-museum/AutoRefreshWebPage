cba34b16d3dc08ab428b09b73935811c3c83fa78
Wayne <liuzhy520@gmai.com>
2016-10-30 15:48:18 +0800

    no message

--- useBrowser2RefreshPage.py
+++ useBrowser2RefreshPage.py
@@ -1,9 +1,8 @@
-import webbrowser  
-import time  
-import os  
-  
+import webbrowser
+import time
+import os
+
 url = 'http://c.m.163.com/news/l/102331.html?w=2'
-for j in range(200):  
-	webbrowser.open_new(url)
-	print('%s %d' % ('count :' , j))
-	
+for j in range(200):
+    webbrowser.open_new(url)
+    print('%s %d' % ('count :', j))
